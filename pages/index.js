0892e3ccc5dd15a521ef2db6a1fc6cd1561d82b1
Helmi Lakhder <102436033+HelmiDev03@users.noreply.github.com>
2023-05-11 22:27:31 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -129,7 +129,7 @@ export default function Home() {
                 Fork it on GitHub
               </Link>{" "}
             
-              .
+              
             </div>
           </div>
 
